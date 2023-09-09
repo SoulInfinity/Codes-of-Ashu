4f0eab63f73fbb5eaa2a99d57799dd90b026c281
SoulInfinity <ashutoshdas3742@gmail.com>
2023-09-09 11:43:09 +0530

    Java codes

---
+++ Super_keyword1.java
@@ -0,0 +1,20 @@
+
+
+public class Super_keyword1 {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.display();
+    }
+}
+class Animal
+{
+    String color = "White";
+}
+class Dog extends Animal{
+    String color = "Black";
+    void display()
+    {
+        System.out.println(color);
+        System.out.println(super.color);
+    }
+}
